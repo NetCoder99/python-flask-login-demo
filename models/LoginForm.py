a77871f8cdea3a127ac5e75dfacc51597b013a46
vs001 <vs001@cox.net>
2023-04-27 22:29:03 -0700

    Save wip

--- models/LoginForm.py
+++ models/LoginForm.py
@@ -1,3 +1,6 @@
+# --------------------------------------------------------------
+# Various fields to support login form
+# --------------------------------------------------------------
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Length
--- setup.py
+++ setup.py
@@ -1,13 +1,5 @@
-# from flask import Flask
-# from flask_bootstrap import Bootstrap
+# --------------------------------------------------------------
+# Init sqlalchemy here to prevent circular references
+# --------------------------------------------------------------
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
-
-
-#
-# app = Flask(__name__)
-# app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-# bootstrap = Bootstrap(app)
-# db = SQLAlchemy(app)
-#
