b72bac96829d7e5a05ad21dc7e34ee053941478f
vs001 <vs001@cox.net>
2023-06-11 15:02:23 -0700

    Added some Ajax

--- data/getData.py
+++ data/getData.py
@@ -1,5 +1,6 @@
 import json
 import os
+from time import sleep
 
 from flask import Blueprint, render_template
 
@@ -10,9 +11,17 @@ def products_api():
     ## http://127.0.0.1:5000/data/products
     absolute_path = os.path.dirname(__file__)
     full_path     = os.path.join(absolute_path, "products.json")
+
+    sleep(2)
+
     with open(full_path) as products_file:
-        return json.load(products_file)
+        json_data = json.load(products_file)
+        return json_data['products']
 
 @getData_blueprint.route('/products', methods=['GET'])
 def products():
     return render_template('products.html')
+
+@getData_blueprint.route('/products_ajax', methods=['GET'])
+def products_ajax():
+    return render_template('products_ajax.html')
